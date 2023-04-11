3eb02736164210f98e2b3e77a86ac04d0d0fc34f
hani2057 <hanikim2057@gmail.com>
2023-04-11 23:42:19 +0900

    0411

--- programmers/2304/0411/gamemap.js
+++ programmers/2304/0411/gamemap.js
@@ -1,27 +1,31 @@
 function solution(maps) {
-  const n = maps[0].length;
-  const m = maps.length;
+  const n = maps[0].length - 1; // 가로 최대 인덱스
+  const m = maps.length - 1; // 세로 최대 인덱스
+  maps[0][0] = 0;
 
   const dx = [1, -1, 0, 0];
   const dy = [0, 0, 1, -1];
 
-  const visited = [[0, 0, 0]];
-  const queue = [[0, 0, 0]];
+  const queue = [[0, 0, 1]];
 
-  while (queue) {
-    const [x, y, res] = queue.shift();
+  while (queue.length) {
+    const [y, x, res] = queue.shift();
+
+    // 목적지에 도착하면 저장된 거리를 리턴
     if (x === n && y === m) return res;
 
     for (let i = 0; i < 4; i++) {
       const nx = x + dx[i];
       const ny = y + dy[i];
 
-      if (maps[x][y] && )
+      if (nx >= 0 && nx <= n && ny >= 0 && ny <= m && maps[ny][nx]) {
+        queue.push([ny, nx, res + 1]);
+        maps[ny][nx] = 0;
+      }
     }
   }
 
-  var answer = 0;
-  return answer;
+  return -1;
 }
 
 const maps1 = [
